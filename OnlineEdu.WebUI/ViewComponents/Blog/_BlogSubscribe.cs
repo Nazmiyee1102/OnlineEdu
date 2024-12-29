a51ab5127db1b5cc20274a3c9c2be075f9497b2a
Nazmiyee1102 <159723558+Nazmiyee1102@users.noreply.github.com>
2024-12-29 18:13:43 +0300

    #Recent Blogs & Subscribe

---
+++ OnlineEdu.WebUI/ViewComponents/Blog/_BlogSubscribe.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.WebUI.DTOs.SubscriberDtos;
+using OnlineEdu.WebUI.Helpers;
+
+namespace OnlineEdu.WebUI.ViewComponents.Blog
+{
+    public class _BlogSubscribe : ViewComponent
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _client.GetFromJsonAsync<List<ResultSubscriberDto>>("blogs/GetLast4Blogs");
+            return View();
+        }
+    }
+}
